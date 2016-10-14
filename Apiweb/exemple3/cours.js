3c0e258b7ebbcbb75f113e3e94e66aeb568e46f7
samir <samir>
2016-10-14 12:27:06 +0200

    ajout

--- Apiweb/exemple3/cours.js
+++ Apiweb/exemple3/cours.js
@@ -4,8 +4,8 @@ ajaxGet("https://api.wunderground.com/weather/api/2a39dcdf9fae358a/conditions/q/
   // Récupération de certains résultats
   var temperature = meteo.current_observation.temp_c;
   var humidite = meteo.current_observation.relative_humidity;
+
   var imageUrl = meteo.current_observation.icon_url;
-  // Affichage des résultats
   var conditionsElt = document.createElement("div");
   conditionsElt.textContent = "Il fait actuellement " + temperature +
       "°C et l'humidité est de " + humidite;
